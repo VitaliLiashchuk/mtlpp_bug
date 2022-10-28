8250d6d2bd3b5c94b07a7b0f0d1fd8083b8ad5bb
Vitali Liashchuk <vl@adverty.com>
2022-10-28 10:49:40 +0200

    Revert "mtlpp reference added"
    This reverts commit 29d5a0b5830ad64ea3f516bcba5a229548d9cabb.

--- mtlpp_bug/Source/mtlpp_bugEditor.Target.cs
+++ mtlpp_bug/Source/mtlpp_bugEditor.Target.cs
@@ -10,7 +10,6 @@ public class mtlpp_bugEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "mtlpp_bug" } );
-        AddEngineThirdPartyPrivateStaticDependencies(Target, "MTLPP");
         if (Target.Platform == UnrealTargetPlatform.Mac)
         {
             bOverrideBuildEnvironment = true;
